8565cc1ff5a35d47f8367391d521aac8a1e4ac2b
Andrea Vecchiato <vecchiato.websites@gmail.com>
2020-07-04 23:30:47 +0200

    Definitivo scritto da me

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,41 +6,24 @@ button.addEventListener('click', addToDo);
 taskList.addEventListener('click', removeTask);
 
 function addToDo(e) {
-    console.log('ciao')
 
-    if (input_text.value === "") {
-        alert('Inserisci un nota');
+    if (input_text.value === '') {
+        alert('Inserisci una nota');
     }
     
-    // Creare nuovo li e aggiungere classe
     const li = document.createElement('li');
-    li.className = 'task';  // tutti i li creati avranno come classe task
+    li.className = 'task';
+    taskList.appendChild(li);
 
-    // mettiamo il testo dell'input nel li con appendChild e con createTextNode lo andiamo ad inserire 
     li.appendChild(document.createTextNode(input_text.value));
 
-    // creare link
     const link = document.createElement('a');
-
-    // aggiungere classe a <a>
-    link.className = "delete-todo";
-
-    // aggiungi x al <a></a>
-
     link.innerHTML = '<p>X</p>';
-
-    // mettere link nel li
+    link.className = 'delete-todo'
     li.appendChild(link);
 
-    // mettere il li dentro l'ul
-    taskList.appendChild(li);
-    
-    // ripuliamo l'input ogni volta che abbiamo inserito una todo
-    input_text.value = "";
+    input_text.value = '';
 
-    
-
-    console.log(taskList);
     e.preventDefault();
 }
 
@@ -48,4 +31,4 @@ function removeTask(e) {
     if (e.target.parentElement.classList.contains('delete-todo')) {
         e.target.parentElement.parentElement.remove();
     }
-}
+} 
